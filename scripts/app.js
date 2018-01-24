6ab29447bea8c25697667b26a2b72640f840f5d9
Stephanie Patton <stephaniepatton@stephanies-mbp.attlocal.net>
2018-01-23 20:25:21 -0600

    Profile site- first commit

---
+++ scripts/app.js
@@ -0,0 +1,9 @@
+function openSideMenu() {
+  document.getElementById('nav-bar').style.width = '50%';
+  document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+function closeSideMenu() {
+  document.getElementById('nav-bar').style.width = '0';
+  document.body.style.backgroundColor = "rgba(0,0,0,0)";
+}
