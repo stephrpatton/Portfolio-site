a10978027db4d003ce7fb6db0731619b02439f50
Stephanie Patton <stephaniepatton@stephanies-mbp.attlocal.net>
2018-01-26 23:34:45 -0600

    setup landing page

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,46 @@
+//Landing Page
+
+const header = document.querySelector('.header-section');
+const left = document.querySelector('.portfolio-section');
+const right = document.querySelector('.service-section');
+const container = document.querySelector('.container');
+const footer = document.querySelector('.footer-section');
+
+left.addEventListener('mouseenter', () => {
+  container.classList.add('hover-left');
+});
+
+left.addEventListener('mouseleave', () => {
+  container.classList.remove('hover-left');
+});
+
+right.addEventListener('mouseenter', () => {
+  container.classList.add('hover-right');
+});
+
+right.addEventListener('mouseleave', () => {
+  container.classList.remove('hover-right');
+});
+
+header.addEventListener('mouseenter', () => {
+  container.classList.add('hover-up');
+});
+
+header.addEventListener('mouseleave', () => {
+  container.classList.remove('hover-return');
+});
+
+footer.addEventListener('mouseenter', () => {
+  footer.classList.add('hover-down');
+});
+
+footer.addEventListener('mouseleave', () => {
+  footer.classList.remove('hover-return');
+});
+
+
+// Mobile View Slide out menu
+
 function openSideMenu() {
   document.getElementById('nav-bar').style.width = '50%';
   document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
