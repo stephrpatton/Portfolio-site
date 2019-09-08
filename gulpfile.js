bed533db3783e2c0446571d8a9e237f216cd4f10
Stephanie Patton <stephaniepatton@stephanies-air.attlocal.net>
2019-09-08 18:25:19 -0500

    added projects

--- gulpfile.js
+++ gulpfile.js
@@ -1,43 +1,50 @@
-let gulp = require('gulp');
-let sass = require('gulp-sass');
-let cleanCSS = require('gulp-clean-css');
-let sourcemaps = require('gulp-sourcemaps');
-let autoprefixer = require('gulp-autoprefixer');
-let watch = require('gulp-watch');
-let browserSync = require('browser-sync');
+let gulp = require("gulp");
+let sass = require("gulp-sass");
+let cleanCSS = require("gulp-clean-css");
+let sourcemaps = require("gulp-sourcemaps");
+let autoprefixer = require("gulp-autoprefixer");
+let watch = require("gulp-watch");
+let browserSync = require("browser-sync");
 let reload = browserSync.reload;
 
-gulp.task('sass', function () {
-  return gulp.src('./sass/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(autoprefixer({
-            browsers: ['last 2 versions'],
-            cascade: false
-        }))
-    .pipe(gulp.dest('./css'))
-    .pipe(browserSync.reload({
-      stream: true
-    }));
+gulp.task("sass", () => {
+  return gulp
+    .src("./sass/**/*.scss")
+    .pipe(sass().on("error", sass.logError))
+    .pipe(
+      autoprefixer({
+        browsers: ["last 2 versions"],
+        cascade: false
+      })
+    )
+    .pipe(gulp.dest("./css"))
+    .pipe(
+      browserSync.reload({
+        stream: true
+      })
+    );
 });
 
-
-gulp.task('browser-sync', function(){
+gulp.task("browser-sync", () => {
   browserSync.init({
-    server: './',
+    server: "./",
     notify: false,
     open: true
   });
 });
 
-gulp.task('sass:minify', function(){
-  return gulp.src('./css/*.css')
+gulp.task("sass:minify", () => {
+  return gulp
+    .src("./css/*.css")
     .pipe(sourcemaps.init())
     .pipe(cleanCSS())
     .pipe(sourcemaps.write())
-    .pipe(gulp.dest('./css'))
+    .pipe(gulp.dest("./css"));
 });
 
-gulp.task('default', ['sass', 'browser-sync', 'sass:minify'], function () {
-  gulp.watch('./sass/**/*.scss', ['sass']).on('change', browserSync.reload);
-  gulp.watch("./*.html").on('change', browserSync.reload);
+gulp.task("default", () => {
+  gulp
+    .watch("./sass/**/*.scss", gulp.series("sass"))
+    .on("change", browserSync.reload);
+  gulp.watch("./*.html").on("change", browserSync.reload);
 });
--- scripts/app.js
+++ scripts/app.js
@@ -1,38 +1,33 @@
 //Landing Page
-const left = document.querySelector('.portfolio-section');
-const right = document.querySelector('.service-section');
-const container = document.querySelector('.container');
-const button = document.querySelector('.button')
+const left = document.querySelector(".portfolio-section");
+const right = document.querySelector(".service-section");
+const container = document.querySelector(".container");
+const button = document.querySelector(".button");
 
-left.addEventListener('mouseenter', () => {
-  container.classList.add('hover-left');
+// left.addEventListener('mouseenter', () => {
+//   container.classList.add('hover-left');
+// });
+
+// left.addEventListener('mouseleave', () => {
+//   container.classList.remove('hover-left');
+// });
+
+right.addEventListener("mouseenter", () => {
+  container.classList.add("hover-right");
 });
 
-left.addEventListener('mouseleave', () => {
-  container.classList.remove('hover-left');
+right.addEventListener("mouseleave", () => {
+  container.classList.remove("hover-right");
 });
-
-right.addEventListener('mouseenter', () => {
-  container.classList.add('hover-right');
-});
-
-right.addEventListener('mouseleave', () => {
-  container.classList.remove('hover-right');
-});
-
-
-
-
-
 
 // Mobile View Slide out menu
 
 function openSideMenu() {
-  document.getElementById('nav-bar').style.width = '50%';
+  document.getElementById("nav-bar").style.width = "50%";
   document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
 }
 
 function closeSideMenu() {
-  document.getElementById('nav-bar').style.width = '0';
+  document.getElementById("nav-bar").style.width = "0";
   document.body.style.backgroundColor = "rgba(0,0,0,0)";
 }
